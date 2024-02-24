ba8ab6fc01352e62b5bf2377be9b8338d456d6bc
Manish bisht <manishsinghbisht10@gmail.com>
2024-02-24 16:47:32 +0530

    addition functionality to progress div

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
-const next = document.getElementById("nexy");
+const next = document.getElementById("next");
 const circle = document.querySelectorAll(".circle");
 
 let active = 1;
@@ -16,7 +16,7 @@ next.addEventListener("click", function () {
 
 prev.addEventListener("click", function () {
   active--;
-  if (active < circle.length) {
+  if (active < 1) {
     active = 1;
   }
   console.log(active);
@@ -28,7 +28,21 @@ function update() {
     if (idx < active) {
       circle.classList.add("active");
     } else {
-      circle.classList.add("active");
+      circle.classList.remove("active");
     }
   });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circle.length - 1)) * 100 + "%";
+
+  if (actives.length === 1) {
+    prev.disabled = true;
+  } else if (actives.length === circle.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
